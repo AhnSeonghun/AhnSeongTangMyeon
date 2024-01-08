1622e63bf017a7282e65fc48aa25b3d5be2ae534
AhnSeonghun <888seonghun888@gmail.com>
2024-01-08 11:10:55 +0900

    11시 10분

--- AhnSeongTangMyeon_1/Assets/Scripts/Temp_GameObject.cs
+++ AhnSeongTangMyeon_1/Assets/Scripts/Temp_GameObject.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class Temp_GameObject : MonoBehaviour
 {
     public int hp;
+    public float moveSpeed = 1.0f;
+    public Vector3 pos = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
         hp = 50;
+
+        pos = new Vector3(20, 20, 20);
+        transform.position = pos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        MoveCube();
+
+        if(Input.GetKeyDown(KeyCode.Space)) // 키가 내려갔을 때
         {
-            hp -= 1;
+            moveSpeed++;
+        }
+    }
+
+    void MoveCube()
+    {
+        if(Input.GetKey(KeyCode.UpArrow)) // 키가 눌러져 있을 때
+        {
+            transform.position += new Vector3(0.0f, moveSpeed * Time.deltaTime, 0.0f);
+        }
+        if(Input.GetKey(KeyCode.DownArrow)) // 키가 눌러져 있을 때
+        {
+            transform.position += new Vector3(0.0f, -moveSpeed * Time.deltaTime, 0.0f);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)) // 키가 눌러져 있을 때
+        {
+            transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)) // 키가 눌러져 있을 때
+        {
+            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0.0f, 0.0f);
         }
     }
 }
